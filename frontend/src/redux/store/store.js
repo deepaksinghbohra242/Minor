50ec1cb5d2ebc9a45d945e1c952c22b567074a01
Deepak Singh Bohra <deepaksinghbohra242@gmail.com>
2023-04-29 15:35:08 +0530

    ho ja

--- frontend/src/redux/store/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import usersReducer from "../slices/users/userSlices";
-const store = configureStore({
-  reducer: {
-    users: usersReducer,
-  },
-});
-
-export default store;
