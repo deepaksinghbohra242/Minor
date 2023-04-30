ba9eafb644fc699d50e5e3c4b0d34adbdad7180b
Deepak Singh Bohra <deepaksinghbohra242@gmail.com>
2023-04-30 09:44:41 +0530

    navbar done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,12 @@ import {BrowserRouter, Routes ,Route } from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage";
 import Register from "./components/User/Register/Register";
 import Login from "./components/User/Login/Login";
-
+import Navbar from "./components/Navigation/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/register" element={<Register />} />
---
+++ frontend/src/components/Navigation/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import AdminNavbar from "./Admin/AdminNavbar";
+import { useSelector } from "react-redux";
+import PrivateNavbar from "./Private/PrivateNavbar";
+import PublicNavbar from "./Public/PublicNavbar";
+
+const Navbar = () => {
+  //get user from store
+  const state = useSelector(state => state.users)
+  const {userAuth} = state;
+  const isAdmin = userAuth?.isAdmin;
+  return (
+    <>
+      {!userAuth ? <PublicNavbar /> : userAuth ? <PrivateNavbar /> : isAdmin && <AdminNavbar />}
+    </>
+  );
+};
+
+export default Navbar;
